e0ec78c6025c3e2eab75a6ad371de4c07de36ade
GamingLogics11 <44622087+GamingLogics11@users.noreply.github.com>
2022-11-22 15:39:14 -0500

    Update check_nilpotent.py

--- check_nilpotent.py
+++ check_nilpotent.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy import linalg as LA
+
 
 def create_matrix():
     dimension = int(input("Please enter matrix dimension: "))
@@ -14,35 +14,36 @@ def create_matrix():
                 initial[i][j] = value
         return initial
 
+
 def check_nilpotent_iterative():
     """ Sam's homemade solution """
     index = 1
     zero_count = 0
-    starting = to_check = create_matrix() # setting starting for a static variable for later, to check will change
+    starting = to_check = create_matrix()  # setting starting for a static variable for later, to check will change
     dim = to_check.ndim
-
+    result = starting
     # this for loop multiplies the matrix with each iteration
     for i in range(dim):
-        result = to_check
         # these nested for loops iterate through the whole matrix
         for j in range(dim):
             for k in range(dim):
-                if result[j][k] == 0:
+                if to_check[j][k] == 0:
                     zero_count += 1
-                    if zero_count == dim * dim:
-                        print(f'{starting} is nilpotent at degree {index}')
-                        return 1
-                    else:
-                        zero_count = 0
-                        pass
                 else:
-                    pass
+                    continue
+        if zero_count == dim * dim:
+            print(f'{starting} is nilpotent at degree {index}')
+            return 1
+        else:
+            zero_count = 0
+            pass
         result = np.matmul(to_check, starting)
         index += 1
         to_check = result
         # end of matrix check, loop back and multiply again
     print(f"{starting} is not nilpotent")
     return -1
-    
-    if __name__ == '__main__':
+
+
+if __name__ == '__main__':
     check_nilpotent_iterative()
